39e9d53becaab3ba2f15248f66cf28f294765b65
AkkiKrsingh2005 <ak508887@gmail.com>
2026-04-02 07:02:04 +0530

    Precision fix for LangChain v0.3+ import paths for cloud

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_chroma import Chroma
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from dotenv import load_dotenv
